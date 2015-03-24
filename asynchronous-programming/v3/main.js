864eb7d466f4578007346e3a6f27ffb63a07eba4
hushicai <bluthcy@gmail.com>
2015-03-24 18:02:29 +0800

    update async

---
+++ asynchronous-programming/v3/main.js
@@ -0,0 +1,34 @@
+var finder = {
+    run: function (records) {
+        var self = this;
+        setTimeout(function () {
+            records.push(3, 4);
+           self.emit('done', records);
+        }, 1000);
+    }
+};
+var processor = {
+    run: function (records) {
+        var self = this;
+        setTimeout(function () {
+            records.push(5, 6);
+            self.emit('done', records);
+        }, 1000);
+    }
+};
+
+var EventEmitter = require('events').EventEmitter;
+
+// mixin
+finder.__proto__ = EventEmitter.prototype;
+processor.__proto__ = EventEmitter.prototype;
+
+finder.on('done', function (records) {
+    processor.run(records);
+});
+processor.on('done', function (records) {
+    console.log(records);
+});
+
+// run
+finder.run([1, 2]);
